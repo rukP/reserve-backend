f5a9ba7674c1a47b2a2381dcb5b6b7b575a3fc03
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-05-17 14:27:05 +0200

    fix regexp error

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { errorHandler } from './middlewares/errorMiddleware';
-import { logger } from './utils/logger';
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import { AppError, errorHandler } from "./middlewares/errorMiddleware";
+import { logger } from "./utils/logger";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  logger.info('Root route was hit');
-  res.send('Parking API is running...');
+app.get("/", (req, res) => {
+  logger.info("Root route was hit");
+  res.send("Parking API is running...");
 });
 
-// Test bad route (optional)
-app.get('/test-error', (req, res) => {
-  throw new Error('This is a test error');
+// CATCH-ALL ROUTE
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error = new AppError(`Can't find ${req.originalUrl} on this server!`, 404);
+  next(error);
 });
 
-app.use('*', (req, res) => {
-  logger.warn(`404 Not Found - ${req.originalUrl}`);
-  res.status(404).json({ success: false, message: 'Route not found' });
-});
 
 app.use(errorHandler);
 
